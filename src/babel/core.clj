144f1719d7307c728820538550d3798248cc84d0
unwarysage <shawx538@morris.umn.edu>
2018-01-31 18:22:54 -0600

    possible print-> reprint->rererereprint funhouse

--- src/babel/core.clj
+++ src/babel/core.clj
@@ -4,22 +4,29 @@
          '[clojure.spec.test.alpha :as stest])
 
 
-
+(def counter (atom 0))
 
 (defn instrument-after-each
   [handler]
   (fn [inp-message]
     (do
       (stest/instrument)
-      (handler inp-message))))
+      (swap! counter (fn [nope] (identity
+        {:str (str inp-message)
+        :class (class inp-message)
+        :identity (assoc inp-message :sumin 5)})))
+      #_(print @counter)
+      (print   inp-message)
+      (print "")
+      (handler  inp-message))))
 
-#_(clojure.tools.nrepl.middleware/set-descriptor! #'print-side-effect
+(clojure.tools.nrepl.middleware/set-descriptor! #'instrument-after-each
         {:requires #{}
          :expects #{"eval"}
          :handles {}})
 
 
-::the below are just debug things
+;;the below are just debug things
 (defn blipper
   [inp]
   "blip")
