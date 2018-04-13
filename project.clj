3c89ef6dacba59fbea26dce32592d51122eb9a37
unwarysage <unwary@comcast.net>
2018-04-13 15:26:40 -0500

    add basic expectations tests

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.nrepl "0.2.13"]]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [expectations "2.2.0-rc3"]]
+  :plugins [[lein-expectations "0.0.8"]]
   :repl-options {:nrepl-middleware
                  [babel.middleware/interceptor]})
---
+++ test/babel/processor_test.clj
@@ -0,0 +1,17 @@
+(ns babel.processor-test
+  (:require [expectations :refer :all]
+            [babel.processor :refer :all]))
+
+;;functions to wrap error messages to look like nREPL messages
+(defn msg-from-err [inp-error] {:err inp-error})
+(defn err-from-msg [inp-message] (inp-message :err))
+
+(def divide-by-zero-error
+  (msg-from-err "ArithmeticException Divide by zero  clojure.lang.Numbers.divide (Numbers.java:163)"))
+
+(expect #"Tried to divide by zero" (err-from-msg (modify-errors divide-by-zero-error)))
+
+(def unable-to-resolve-symbol-error
+  (msg-from-err "CompilerException java.lang.RuntimeException: Unable to resolve symbol: monkey in this context, compiling:(/tmp/form-init8371041852706021326.clj:1:1)"))
+
+(expect #"Name (.*) is undefined" (err-from-msg (modify-errors unable-to-resolve-symbol-error)))
