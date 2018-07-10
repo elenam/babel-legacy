7c62b367bb86c070fa5f3efb5aa2ddef6f9d2bbe
plumstyle <anxxx154@morris.umn.edu>
2018-07-10 01:15:16 -0400

    remodeled the prototype from 2 open repl to only 1 open repl.

--- src/babel/processor.clj
+++ src/babel/processor.clj
@@ -4,11 +4,24 @@
              [errors.messageobj :as m-obj]
              [errors.prettify-exception :as p-exc]))
 
+;;an atom that record original error response
+(def recorder (atom {:msg ""}))
+
+;;reset the recorder
+(defn reset-recorder
+  []
+  (def recorder (atom {:msg ""})))
+
+;;update recorded message
+(defn update-recorder
+  [inp-message]
+  (swap! recorder assoc :msg inp-message))
+
 (defn modify-errors "takes a nREPL response, and returns a message with the errors fixed"
   [inp-message]
   (if (contains? inp-message :err)
     ;;replace the assoced value with a function call as needed.
-    (assoc inp-message :err (m-obj/get-all-text (:msg-info-obj (p-exc/process-spec-errors (inp-message :err)))))
+    (assoc inp-message :err (m-obj/get-all-text (:msg-info-obj (p-exc/process-spec-errors (do (update-recorder (:err inp-message)) (inp-message :err))))))
     ;(assoc inp-message :err (str (inp-message :err))) ;; Debugging
     inp-message))
 
--- src/babel/testingtool.clj
+++ src/babel/testingtool.clj
@@ -1,20 +1,17 @@
 (ns babel.testingtool
   (:require
    [expectations :refer :all]
-   [clojure.tools.nrepl :as repl]))
+   [clojure.tools.nrepl :as repl]
+   [babel.processor :as processor]))
 
 ;;you need to have launched a nREPL server in babel for these to work.
 ;;this must be the same port specified in project.clj
+;;place (start-log) in the testing file to record testing time in the log
 
-;;you also need to have a (\babel>lein repl :start :port 8888) for the original messages
-;;and have (start-log) in the testing file to record testing time in the log
+;;set server-port
+(def server-port 7888)
 
-;;set a pair of server-ports, one is through our middleware and one is not
-(def server-port1 7888)
-
-(def server-port2 8888)
-
-;;message saver atom
+;;counter atom
 (def counter (atom {:total 1}))
 
 ;;reset atom
@@ -22,19 +19,11 @@
   []
   (def counter (atom {:total 1})))
 
-;;a pair of trap-responses gets the returning message from 2 open repl
-(defn trap-response-port1
+;;trap-response gets the returning message from open repl
+(defn trap-response
   "evals the code given as a string, and returns the list of associated nREPL messages"
   [inp-code]
-  (with-open [conn (repl/connect :port server-port1)]
-    (-> (repl/client conn 1000)
-        (repl/message {:op :eval :code inp-code})
-        doall)))
-
-(defn trap-response-port2
-  "evals the code given as a string, and returns the list of associated nREPL messages"
-  [inp-code]
-  (with-open [conn (repl/connect :port server-port2)]
+  (with-open [conn (repl/connect :port server-port)]
     (-> (repl/client conn 1000)
         (repl/message {:op :eval :code inp-code})
         doall)))
@@ -50,26 +39,31 @@
   "takes code as a string, and returns the error from evaulating it on the nREPL server, or nil"
   [inp-code]
   (swap! counter update-in [:total] inc)
-  (msgs-to-error (trap-response-port1 inp-code)))
+  (msgs-to-error (trap-response inp-code)))
 
 ;;add date to the test log
 (defn start-log
   []
   (spit "./doc/test_log.txt" (str (new java.util.Date) "\n")) :append true)
 
+;;get original error msg
+(defn get-original-error
+  []
+  (:value (first (filter :value (trap-response "(:msg @babel.processor/recorder)")))))
+
 ;;content that is going to be put into the log
 (defn log-content
   [inp-code]
   (if
-    (not= (msgs-to-error (trap-response-port1 inp-code)) nil)
+    (not= (msgs-to-error (trap-response inp-code)) nil)
     (str "#" (:total @counter) ":\n"
          "code input: " inp-code "\n"
-         "modified error: " (clojure.string/trim-newline (msgs-to-error (trap-response-port1 inp-code))) "\n"
-         "original error: " (clojure.string/trim-newline (msgs-to-error (trap-response-port2 inp-code))) "\n\n")
+         "modified error: " (clojure.string/trim-newline (msgs-to-error (trap-response inp-code))) "\n"
+         "original error: " (clojure.string/trim-newline (get-original-error)) "\n\n")
     (str "#" (:total @counter) ":\n"
          "code input: " inp-code "\n"
          "modified error: nil\n"
-         "original error: nil\n")))
+         "original error: nil\n\n")))
 
 ;;save the content into the log file
 (defn save-log
@@ -86,6 +80,7 @@
   [inp-code]
   (do
     (save-log inp-code)
+    (processor/reset-recorder)
     (record-error inp-code)))
 
 (println "babel.testingtool loaded")
