41ba7eb5cd6c3c07dd38727327c0765e4350c005
plumstyle <anxxx154@morris.umn.edu>
2018-07-13 19:25:45 -0400

    finished html logging, need approval.

--- src/babel/testingtool.clj
+++ src/babel/testingtool.clj
@@ -8,6 +8,7 @@
 
 ;;you need to have launched a nREPL server in babel for these to work.
 ;;this must be the same port specified in project.clj
+;;you also need include hiccup dependency in project.clj
 ;;place (start-log) in the testing file to record testing time in the log
 
 ;;set server-port
@@ -43,19 +44,70 @@
   (swap! counter update-in [:total] inc)
   (msgs-to-error (trap-response inp-code)))
 
+
+;;preset html environment
 (defn html-log-preset
   []
-  (html [:title "Test_log"] [:h3 "Test_log: "] [:h4 (new java.util.Date)]))
+  (html [:title "Babel testing log"]
+        [:h3 {:style "padding-top:20px"} "Testing log: "]
+        [:h4 (new java.util.Date)]
+        [:script
+         "function hideModified() {
+           var x = document.getElementsByClassName(\"modifiedError\");
+           if (document.getElementById(\"modified\").checked != true) {
+             var i;
+             for (i = 0; i < x.length; i++) {
+               x[i].style.display='none';
+             }
+             } else {
+             var i;
+             for (i = 0; i < x.length; i++) {
+               x[i].style.display='block';
+             }
+             }
+           }
+           function hideOriginal() {
+             var x = document.getElementsByClassName(\"originalError\");
+             if (document.getElementById(\"original\").checked != true) {
+               var i;
+               for (i = 0; i < x.length; i++) {
+                 x[i].style.display='none';
+               }
+               } else {
+               var i;
+               for (i = 0; i < x.length; i++) {
+                 x[i].style.display='block';
+               }
+               }
+             }
+             function hideDetail() {
+               var x = document.getElementsByClassName(\"errorDetail\");
+               if (document.getElementById(\"detail\").checked != true) {
+                 var i;
+                 for (i = 0; i < x.length; i++) {
+                   x[i].style.display='none';
+                 }
+                 } else {
+                 var i;
+                 for (i = 0; i < x.length; i++) {
+                   x[i].style.display='block';
+                 }
+                 }
+               }"]
+        [:p "Display options:"]
+        [:div
+         [:input#modified {:type "checkbox" :checked true :onclick "hideModified()"} [:a {:style "color:#00AE0C;padding-right:20px"}"modified error"]]
+         [:input#original {:type "checkbox" :checked true :onclick "hideOriginal()"} [:a {:style "color:#D10101;padding-right:20px"} "original error"]]
+         [:input#detail {:type "checkbox" :checked false :onclick "hideDetail()"} "error detail"]]))
 
-
-;;add date to the test log
+;;add date to the txt and html test log
 (defn start-log
   []
   (do
-    (spit "./doc/testhtml.html" (html-log-preset) :append false)
+    (spit "./doc/test_log.html" (html-log-preset) :append false)
     (spit "./doc/test_log.txt" (str (new java.util.Date) "\n")) :append true))
 
-;;get original error msg
+;;get original error msg by key
 (defn get-original-error-by-key
   [key]
   (:value (first (filter :value (trap-response (str "(" key " @babel.processor/recorder)"))))))
@@ -76,34 +128,40 @@
          "original error: nil\n\n"
          "error detail: nil\n\n\n")))
 
-;;save the content into the log file
+;;save the content into the txt log file
 (defn save-log
   [inp-code]
   (spit "./doc/test_log.txt" (log-content inp-code) :append true))
 
-;;read the exsiting log content
+;;read the exsiting txt log content
 (defn read-log
   []
   (println (slurp "./doc/test_log.txt")))
 
+;;html content
 (defn html-content
   [inp-code]
   (if
     (not= (msgs-to-error (trap-response inp-code)) nil)
-    (html [:p "#" (:total @counter) ":<br />"
-               "code input: " inp-code "<br />"
-               "modified error: " (clojure.string/trim-newline (msgs-to-error (trap-response inp-code))) "<br />"
-               "original error: " (clojure.string/trim-newline (get-original-error-by-key :msg)) "<br />"
-               "error detail: "(clojure.string/trim-newline (get-original-error-by-key :detail)) "<br /><br />"])
-    (html [:p "#" (:total @counter) ":<br />"
-               "code input: " inp-code "<br />"
-               "modified error: nil<br />"
-               "original error: nil<br />"
-               "error detail: nil<br /><br />"])))
+    (html [:div
+           [:hr]
+           [:p "#" (:total @counter) ":<br />"]
+           [:p {:style "color:#020793"} "code input: " inp-code "<br />"]
+           [:p {:class "modifiedError" :style "color:#00AE0C"} "modified error: " (clojure.string/trim-newline (msgs-to-error (trap-response inp-code))) "<br />"]
+           [:p {:class "originalError" :style "color:#D10101"} "original error: " (clojure.string/trim-newline (get-original-error-by-key :msg)) "<br />"]
+           [:p {:class "errorDetail" :style "display:none"} "error detail: "(clojure.string/trim-newline (get-original-error-by-key :detail)) "<br /><br />"]])
+    (html [:div
+           [:hr]
+           [:p "#" (:total @counter) ":<br />"]
+           [:p {:style "color:#020793"} "code input: " inp-code "<br />"]
+           [:p {:class "modifiedError" :style "color:#00AE0C"} "modified error: nil<br />"]
+           [:p {:class "originalError" :style "color:#D10101"} "original error: nil<br />"]
+           [:p {:class "errorDetail" :style "display:none"} "error detail: nil<br /><br />"]])))
 
+;;write html content
 (defn write-html
   [inp-code]
-  (spit "./doc/testhtml.html" (html-content inp-code) :append true))
+  (spit "./doc/test_log.html" (html-content inp-code) :append true))
 
 ;;the execution funtion for the tests
 (defn get-error
