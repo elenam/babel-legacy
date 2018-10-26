267dbdbb85d5600f9ccb84cd80b27e28a344c8fd
unwarysage <unwary@comcast.net>
2018-10-25 22:13:04 -0500

    add full object catching, with the following caveats
    undid @plumstyles work. This should be put back before full merge
    has an approximately fifty percent chance to fail per repl bootup.
    I have ideas on how to fix this.
    changes error messages subtly enough that regexes have broken. All errors are default errors now.

--- src/babel/processor.clj
+++ src/babel/processor.clj
@@ -1,43 +1,26 @@
 (ns babel.processor
-  (:require  [clojure.tools.nrepl :as repl]
-             [clojure.spec.test.alpha :as stest]
-             [errors.messageobj :as m-obj]
-             [errors.prettify-exception :as p-exc]
-             [corefns.corefns :as cf]
-             [corefns.instrumentfunctionsfortesting]))
+ (:require [errors.messageobj :as m-obj]
+           [errors.prettify-exception :as p-exc]))
 
-;;an atom that record original error response
-(def recorder (atom {:msg [] :detail []}))
 
-(defn reset-recorder
-  "This function reset the recorder atom"
+
+(defn get-error
   []
-  (reset! recorder {:msg [] :detail []}))
+  ((comp #(% #'clojure.core/*e)
+         deref
+         second
+         first
+         deref
+         deref
+        #(get % 'sessions))
+   (ns-interns `clojure.tools.nrepl.middleware.session)))
 
-(defn update-recorder-msg
-  "takes an unfixed error message, and put it into the recorder"
-  [inp-message]
-  (swap! recorder update-in [:msg] conj inp-message))
-  ;(swap! recorder assoc :msg inp-message))
-
-(defn update-recorder-detail
-  "takes error message details, and put them into the recorder"
-  [inp-message]
-  (swap! recorder update-in [:detail] conj inp-message))
-
-(defn modify-errors
-  "takes a nREPL response, and returns a message with the errors fixed"
-  [inp-message]
+(defn modify-errors [inp-message]
   (if (contains? inp-message :err)
-    ;;replace the assoced value with a function call as needed.
-    (assoc inp-message :err
-      (let [err (inp-message :err)
-            processed (p-exc/process-spec-errors err)]
-            (m-obj/get-all-text (:msg-info-obj (do (update-recorder-detail processed)
-                                                   (update-recorder-msg err)
-                                                   processed)))))
-    ;(assoc inp-message :err (str (inp-message :err))) ;; Debugging
-    ;(assoc inp-message :err (str "\n" inp-message "\n" (p-exc/process-spec-errors (inp-message :err)))) ;; Debugging
-    inp-message))
+      (assoc inp-message :err
+             (let [err (str (get-error) "\n")
+                   processed (p-exc/process-spec-errors err)]
+                  (m-obj/get-all-text (:msg-info-obj processed))))      
+      inp-message))
 
 (println "babel.processor loaded")
