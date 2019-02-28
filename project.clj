cfdcc1eafb412e8ae5be63aaab67ee21ada5ac68
Ethan <ethan.uphoff@gmail.com>
2019-02-27 21:18:22 -0600

    Removed unused dependencies

--- project.clj
+++ project.clj
@@ -6,12 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [expectations "2.2.0-rc3"]
-                 [hiccup "1.0.5"]
-                 [org.onyxplatform/onyx "0.13.3-alpha4"]
-                 [org.onyxplatform/onyx-spec "0.12.7.0"]
-                 [org.clojure/java.jdbc "0.7.8"]]
-                 ;[ring "1.7.0-RC1"]
-                 ;[javax.servlet/servlet-api "2.5"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-expectations "0.0.8"]]
   :repl-options {:nrepl-middleware
                  [babel.middleware/interceptor]
