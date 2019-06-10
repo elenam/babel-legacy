3c3134a69f6f12dda511641310f20784dfcdd9cc
elenam <elenam@morris.umn.edu>
2019-06-10 14:26:31 -0500

    replaced an exception by 'hello'

--- src/babel/middleware.clj
+++ src/babel/middleware.clj
@@ -13,15 +13,25 @@
   [handler]
   (fn [inp-message]
     (let [transport (inp-message :transport)
-          sess (inp-message :session)
-          dummy (reset! track {:session sess})]
+          sess (inp-message :session)]
+          ;dummy (reset! track {:session sess})]
       (handler (assoc inp-message :transport
                       (reify Transport
                         (recv [this] (.recv transport))
                         (recv [this timeout] (.recv transport timeout))
-                        (send [this msg]     (.send transport (processor/modify-errors msg)))))))))
+                        (send [this msg]     (.send transport msg))))))));(processor/modify-errors msg)))))))))
                         ;(send [this msg]     (.send transport msg))))))))
 
 ;;sets the appropriate flags on the middleware so it is placed correctly
 (nrepl.middleware/set-descriptor! #'interceptor
                                                 {:expects #{"eval"} :requires #{} :handles {}})
+
+;; I don't seem to be able to bind this var in middleware.
+;; Running (setup) in repl does the trick.
+(defn setup-exc []
+  (set! nrepl.middleware.caught/*caught-fn* #(do
+    (reset! track {:e %})
+    (clojure.main/repl-caught (Throwable. "hello"))
+    (clojure.repl/pst % 5))))
+
+;(setup)
