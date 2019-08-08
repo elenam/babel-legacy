5280157437f48c4f568496b9724dc5499d16ca74
elenam <elenam@morris.umn.edu>
2019-08-08 14:02:49 -0500

    parentheses processing works on some (not all) cases

--- test/babel/macro_test.clj
+++ test/babel/macro_test.clj
@@ -17,23 +17,24 @@
                 (def file-name "this file")
                 (:file (meta #'file-name)))))
 
-(expect "Syntax problems with (let((+ (# (* %1 3 ) ) 2 ) g )7 )):
-In place of (+(# (* %1 3 ) ) 2 ) the following are allowed: a name or a vector or a hashmap"
+(expect "Syntax problems with (let [(+ ((#) (* %1 3)) 2) g] 7):
+In place of + ((#) (* %1 3)) 2 the following are allowed: a name or a vector or a hashmap"
 (log/babel-test-message "(let [(+ #(* %1 3) 2) g] 7)"))
 
-(expect "Syntax problems with (let((+ (# (* %1 3 ) ) 2 ) g )7 )):
-In place of (+(# (* %1 3 ) ) 2 ) the following are allowed: a name or a vector or a hashmap"
-(log/babel-test-message "(let [(+ #(* %1 3) 2) g] 7)"))
+(expect "Syntax problems with (let [((:a 1)) g] 7):
+In place of (:a 1) the following are allowed: a name or a vector
+In place of :a the following are allowed: a name or a vector or a hashmap
+In place of 1 the following are allowed: a vector"
+(log/babel-test-message " (let [{:a 1} g] 7)"))
 
-(expect "Syntax problems with (when-first(11 1 )):\nIn place of (11 1 ) the following are allowed: unknown type" (log/babel-test-message "(when-first 1 1 1)"))
+(expect "Syntax problems with (when-first 1 1 1):
+In place of 1 1 1 the following are allowed: unknown type" (log/babel-test-message "(when-first 1 1 1)"))
 
-(expect "Syntax problems with (let((+ x 1 ) g )g )):
-In place of (+x 1 ) the following are allowed: a name or a vector or a hashmap"
+(expect "Syntax problems with (let [(+ x 1) g] g):
+In place of + x 1 the following are allowed: a name or a vector or a hashmap"
 (log/babel-test-message "(defn macro [s] (let [(+ x 1) g] g))"))
 
 (expect "Expected a number, but a function was given instead." (log/babel-test-message "(let [g (+ #(* %1 3) 2)] 7)"))
 
-(expect "Syntax problems with (let((+ 1 2 ) (+ 1 2 ) )(+ 1 2 ) )):
-In place of (+1 2 ) the following are allowed: a name or a vector or a hashmap" (log/babel-test-message "(let [(+ 1 2) (+ 1 2)] (+ 1 2))"))
-
-(expect "Don't know how to create a sequence from a keyword." (log/babel-test-message "(let [{:a 1} g] 7)"))
+(expect "Syntax problems with (let [(+ 1 2) (+ 1 2)] (+ 1 2)):
+In place of + 1 2 the following are allowed: a name or a vector or a hashmap" (log/babel-test-message "(let [(+ 1 2) (+ 1 2)] (+ 1 2))"))
