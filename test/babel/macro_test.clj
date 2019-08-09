b01cfda31f964dce90d269cef218377cc67b25c9
Hoomz Damte <damte005@avenger.morris.umn.edu>
2019-08-09 13:33:24 -0500

    tests for if-let

--- test/babel/macro_test.clj
+++ test/babel/macro_test.clj
@@ -17,8 +17,8 @@
                 (def file-name "this file")
                 (:file (meta #'file-name)))))
 
-(expect "Syntax problems with (let [(+ (# (* %1 3)) 2) g] 7):
-In place of + (# (* %1 3)) 2 the following are allowed: a name or a vector or a hashmap"
+(expect "Syntax problems with (let [(+ (#(* %1 3)) 2) g] 7):
+In place of + (#(* %1 3)) 2 the following are allowed: a name or a vector or a hashmap"
 (log/babel-test-message "(let [(+ #(* %1 3) 2) g] 7)"))
 
 (expect "Syntax problems with (let [{:a 1} g] 7):
@@ -38,3 +38,15 @@ In place of + x 1 the following are allowed: a name or a vector or a hashmap"
 
 (expect "Syntax problems with (let [(+ 1 2) (+ 1 2)] (+ 1 2)):
 In place of + 1 2 the following are allowed: a name or a vector or a hashmap" (log/babel-test-message "(let [(+ 1 2) (+ 1 2)] (+ 1 2))"))
+
+(expect "Syntax problems with (let [[[3 4] [5 6]] y] ()):
+In place of [3 4] [5 6] the following are allowed: a name or a hashmap" (log/babel-test-message "(let [[[3 4] [5 6]] y])"))
+
+(expect "Syntax problems with (let [(\"chromosome\" (\"x\") \"y\") z] ()):\nIn place of \"chromosome\" (\"x\") \"y\" the following are allowed: a name or a vector or a hashmap"
+(log/babel-test-message "(let [#{\"chromosome\" [\"x\"] \"y\"} z])"))
+
+(expect "if-let has too many parts here: (if-let(x false y true) \"then\" \"else\") The extra parts are: y true"
+(log/babel-test-message "(if-let [x false y true] \"then\" \"else\")"))
+
+(expect "if-let has too many parts here: (if-let((w n) (re-find a(d+)x \"aaa123xxx\")) (w n) :not-found :x) The extra parts are: :x"
+(log/babel-test-message "(if-let [[w n] (re-find #\"a(d+)x\" \"aaa123xxx\")] [w n] :not-found :x)"))
