bf11014cce211b579e8d899727d13848beb849e1
Elena Machkasova <elenam@morris.umn.edu>
2020-02-12 17:03:09 -0600

    Deleted more functions in utils; reorganized

--- src/errors/utils.clj
+++ src/errors/utils.clj
@@ -11,15 +11,18 @@
   [val-str]
   (if (= val-str "") "" (str " " val-str)))
 
+(defn label-vect-maps
+  "Takes a vector of maps and returns it with an extra key/val pair added to each entry:
+  :n and the index in the original vector.
+   For instance, given [{:a 1} {:b 0} {:c 5}] it returns
+   [{:a 1, :n 0} {:b 0, :n 1} {:c 5, :n 2}].
+   Helpful for subsequent sorting since it preserves the index in the original vector."
+  [v-maps]
+  (mapv #(assoc %1 :n %2) v-maps (range)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;; Predicates for handling fn ;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn same-position
-  "Takes two spec problems and returns true if their 'in' is exactly the same
-   and false otherwise"
-  [p1 p2]
-  (= (:in p1) (:in p2)))
 
 (defn- v-prefix=?
   "Returns true if one vector is a prefix of the other one or
@@ -38,20 +41,15 @@
   [value]
   (and (seq? value) (simple-symbol? (first value))))
 
-(defn fn-multi-arity?
-  "Takes a value of a failing spec and returns true if the fn has
-  more than one arity and false otherwise"
-  [value]
-  (let [named? (fn-named? value)
-        n (count value)]
-       (or (and named? (> n 2) (every? seq? (rest value)))
-           (and (not named?) (> n 1) (every? seq? value)))))
-
 (defn fn-has-amp?
   "Takes a value of a failing spec and the 'in' index and returns true if the
   value at the index has & in it and false otherwise"
   [value in]
   (and (vector? (nth value in)) (not (empty? (filter #(= % (symbol '&)) (nth value in))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; Functions for selecting specs ;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn key-vals-match
   "Takes a map of keys and values and returns a function that matches
@@ -112,30 +110,9 @@
   [grouped-probs m]
   (= (get-match grouped-probs m) (sp/select [sp/MAP-VALS sp/ALL] grouped-probs)))
 
-(defn missing-vector-message
-  [grouped-probs value]
-  (if (= 1 (count grouped-probs))
-      (let [no-vectors? (empty? (filter vector? value))
-            val (:val (first (first (vals grouped-probs))))] ; replace by specter?
-            (if no-vectors?
-                (str "A function definition requires a vector of parameters, but was given "
-                     (if (nil? val) "nil" (d/print-macro-arg val)) " instead.")
-                ;; Perhaps should report the failing argument
-                "fn is missing a vector of parameters or it is misplaced."))
-      "Need to handle this case"))
-
-(defn missing-vector-message-seq
-  "Takes a failing spec with a value that's a sequence and returns its
-  error message for a missing vector"
-  [prob value]
-  (let [val (:val prob)
-        no-vectors? (empty? (filter vector? value))]
-        (if no-vectors?
-            (if (#{"fn*" "quote"} (str (first val)))
-                (str "A function definition requires a vector of parameters, but was given " (d/print-macro-arg val) " instead.")
-                (str "A function definition requires a vector of parameters, but was given " (d/print-macro-arg val "(" ")") " instead."))
-            ;; Perhaps should report the failing argument
-            "fn is missing a vector of parameters or it is misplaced.")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;; Various utils for handling specs ;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- not-names->seq
   [val]
@@ -164,6 +141,35 @@
         (if (and has-amp? (empty? not-names-before-amp) (or length-issue-after-amp? not-allowed-after-amp))
             (rest amp-and-after)
             '())))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;; Handling specific spec cases ;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn missing-vector-message
+  [grouped-probs value]
+  (if (= 1 (count grouped-probs))
+      (let [no-vectors? (empty? (filter vector? value))
+            val (:val (first (first (vals grouped-probs))))] ; replace by specter?
+            (if no-vectors?
+                (str "A function definition requires a vector of parameters, but was given "
+                     (if (nil? val) "nil" (d/print-macro-arg val)) " instead.")
+                ;; Perhaps should report the failing argument
+                "fn is missing a vector of parameters or it is misplaced."))
+      "Need to handle this case"))
+
+(defn missing-vector-message-seq
+  "Takes a failing spec with a value that's a sequence and returns its
+  error message for a missing vector"
+  [prob value]
+  (let [val (:val prob)
+        no-vectors? (empty? (filter vector? value))]
+        (if no-vectors?
+            (if (#{"fn*" "quote"} (str (first val)))
+                (str "A function definition requires a vector of parameters, but was given " (d/print-macro-arg val) " instead.")
+                (str "A function definition requires a vector of parameters, but was given " (d/print-macro-arg val "(" ")") " instead."))
+            ;; Perhaps should report the failing argument
+            "fn is missing a vector of parameters or it is misplaced.")))
 
 (defn parameters-not-names
   [prob value]
@@ -216,44 +222,6 @@
               :else (str clause-str
                          (d/print-macro-arg val)
                          " cannot be outside of a function body."))))
-
-(defn clause-number
-  "Takes a vector of failed 'in' entries from a spec error and returns the max one.
-   If none available, returns 0."
-  [ins]
-  (let [valid-ins (filter number? (map first ins))]
-       (if (empty? valid-ins) 0 (apply max valid-ins))))
-
-(defn label-vect-maps
-  "Takes a vector of maps and returns it with an extra key/val pair added to each entry:
-  :n and the index in the original vector.
-   For instance, given [{:a 1} {:b 0} {:c 5}] it returns
-   [{:a 1, :n 0} {:b 0, :n 1} {:c 5, :n 2}].
-   Helpful for subsequent sorting since it preserves the index in the original vector."
-  [v-maps]
-  (mapv #(assoc %1 :n %2) v-maps (range)))
-
-;; Can be used to tell whether a clause is falsely identified,
-;; in particular confused with a function call.
-(defn clause?
-  "Takes an element of value and returns true if it is a function clause
-   and false otherwise."
-  [s]
-  (and (seq? s) (or (empty? s) (vector? (first s)))))
-
-(defn pred-vector?
-  "Takes a predicate of a spec failure and returns true if it is clojure.core/vector?
-  and false otherwise."
-  [pred]
-  (and (symbol? pred) (= #'clojure.core/vector? (resolve pred))))
-
-(defn pick-vector-fail
-  "Takes a list of spec failures and returns the message and the value to
-   report for a failed parameter vector for fn"
-  [probs]
-  (let [vector-fails (filter #(and (pred-vector? (:pred %))
-                                   (v-prefix=? [:fn-tail :arity-1 :params] (:path %))) probs)]
-       (first vector-fails)))
 
 (defn process-nested-error
   "Takes grouped problems of a spec with all 'Extra input' conditions, returns a message based
