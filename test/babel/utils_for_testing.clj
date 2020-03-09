fb911a1157b3dd6c75b3d7bfddfc1a370c534203
Elena Machkasova <elenam@morris.umn.edu>
2020-03-09 11:26:58 -0400

    changed make-pattern to be var-arg, handle regexes. Closed #87

--- test/babel/utils_for_testing.clj
+++ test/babel/utils_for_testing.clj
@@ -4,8 +4,20 @@
 ;### Utils for generating test patterns ##
 ;#########################################
 
-(defn make-pattern
-  "Takes a string and returns a compiled regex pattern for containing
-  that string as a literal. Dot matches newlines."
+(defn- s->pattern
+  "Takes a string or a regular expression and returns a
+  string that can be then combined with other strings to be
+  passed to re-pattern"
   [s]
-  (re-pattern (str "(?s)" (java.util.regex.Pattern/quote s) "(.*)")))
+  (if (string? s)
+      (java.util.regex.Pattern/quote s)
+      (.pattern s)))
+
+(defn make-pattern
+  "Takes a sequence of strings and regexes and returns
+  a compiled regex pattern composed of the strings as literals
+  and the pattrens of the regexes. Dot matches newlines."
+  [& s]
+  (re-pattern (str "(?s)"
+              (apply str (map s->pattern s))
+              "(.*)")))
